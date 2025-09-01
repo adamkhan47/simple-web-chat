518dd89d084ba4e45e7021d990cb838019f27e7b
Adam Khan <adammkhan47@gmail.com>
2025-09-01 10:41:10 -0500

    config stuff

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+// #region config.yaml reading here
+const fileContents = fs.readFileSync('config.yaml', 'utf8');
+const config = yaml.load(fileContents);
+
+const PORT = config.port;
+let LISTENING = config.listen;
+if (LISTENING === "local") {LISTENING = '127.0.0.1';}
+else if (LISTENING === "all") {LISTENING = '0.0.0.0'} 
+const betterConsole = config.alerts;
+let environment = config.environment;
+
+// #endregion 
 
 app.use(express.static(path.join(__dirname, "public")));
-
-
 app.listen(PORT, LISTENING, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Listening on ${LISTENING}`);
